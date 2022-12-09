0e87d0f5b4e61db318f6bd38e9d7e631ae5504df
Abhay Kumar <119960413+Abhay7654586019@users.noreply.github.com>
2022-12-09 17:33:42 +0530

    Add files via upload

---
+++ GreatestNumber.java
@@ -0,0 +1,25 @@
+package firstproject;
+
+public class GreatestNumber {
+	public static void main(String args[])
+	{
+		int a = 1000, b = 2000, c = 3000;  
+		if(a >= b)   
+		{  
+		if(a >= c)  
+		System.out.println("The largest number is: "+a);  
+		else  
+		System.out.println("The largest number is: "+c);  
+		}   
+		else   
+		{  
+		if(b >= c)  
+		System.out.println("The largest number is: "+b);  
+		else  
+		System.out.println("The largest number is: "+c);  
+		}  
+		}    
+		
+	}
+
+
